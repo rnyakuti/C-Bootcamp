3fd41b31c47b889e7921a6522b2df2900b970e26
Rutendo NYAKUTIRA <rnyakuti@c3r2s4.wethinkcode.co.za>
2019-06-03 15:59:02 +0200

    colors and background

--- Day00/ex01/main.cpp
+++ Day00/ex01/main.cpp
@@ -16,66 +16,66 @@ class Contact{
   std::string dark_secret;
   void displayContact()
   {
-    std::cout << "\x1b[93;42mDisplaying \n";
+    std::cout << "\x1b[1;42mDisplaying \n";
 
   }
   void addContact(){
-    std::cout << "\x1b[93;42mAdding contact[+] \n";
-    std::cout << "\x1b[93;42mFirst Name: ";
+    std::cout << "\x1b[1;42mAdding contact[+] \n";
+    std::cout << "\x1b[1;42mFirst Name: ";
     std::getline(std::cin,first_name);
-    std::cout << "\x1b[93;42mLast Name: ";
+    std::cout << "\x1b[1;42mLast Name: ";
     std::getline(std::cin,last_name);
-    std::cout << "\x1b[93;42mNickname: ";
+    std::cout << "\x1b[1;42mNickname: ";
     std::getline(std::cin,nickname);
-    std::cout << "\x1b[93;42mLogin: ";
+    std::cout << "\x1b[1;42mLogin: ";
     std::getline(std::cin,login);
-    std::cout << "\x1b[93;42mPostal Address: ";
+    std::cout << "\x1b[1;42mPostal Address: ";
     std::getline(std::cin,postal_address);
-    std::cout << "\x1b[93;42mEmail Adress: ";
+    std::cout << "\x1b[1;42mEmail Adress: ";
     std::getline(std::cin,email_address);
-    std::cout << "\x1b[93;42mPhone Number: ";
+    std::cout << "\x1b[1;42mPhone Number: ";
     std::getline(std::cin,phonenumber);
-    std::cout << "\x1b[93;42mBirthdate: ";
+    std::cout << "\x1b[1;42mBirthdate: ";
     std::getline(std::cin,birth_date);
-    std::cout << "\x1b[93;42mFavourite meal: ";
+    std::cout << "\x1b[1;42mFavourite meal: ";
     std::getline(std::cin,fave_meal);
-    std::cout << "\x1b[93;42mUnderwear Color: ";
+    std::cout << "\x1b[1;42mUnderwear Color: ";
     std::getline(std::cin,underwear);
-    std::cout << "\x1b[93;42mDarkest Secret: ";
+    std::cout << "\x1b[1;42mDarkest Secret: ";
     std::getline(std::cin,dark_secret);
 
   }
 };
 static void command_options()
 {
-  std::cout << "\x1b[93;42m*********************************************\n";
-  std::cout << "\x1b[93;42m***                   +                   ***\n";
-  std::cout << "\x1b[93;42m***               P H O N E               ***\n";  
-  std::cout << "\x1b[93;42m***                B O O K                ***\n";
-  std::cout << "\x1b[93;42m***                   +                   ***\n";
-  std::cout << "\x1b[93;42m***          <<COMMAND OPTIONS>>          ***\n";
-  std::cout << "\x1b[93;42m***            ADD SERCH EXIT             ***\n";
-  std::cout << "\x1b[93;42m*********************************************\n";    
+  std::cout << "\x1b[1;42m*********************************************\n";
+  std::cout << "\x1b[1;42m***                   +                   ***\n";
+  std::cout << "\x1b[1;42m***               P H O N E               ***\n";  
+  std::cout << "\x1b[1;42m***                B O O K                ***\n";
+  std::cout << "\x1b[1;42m***                   +                   ***\n";
+  std::cout << "\x1b[1;42m***          <<COMMAND OPTIONS>>          ***\n";
+  std::cout << "\x1b[1;42m***            ADD SEARCH EXIT            ***\n";
+  std::cout << "\x1b[1;42m*********************************************\n";    
 }
 
 int main() {
 
-  Contact new_contact;
+  Contact new_contact[8];
   std::string cmd;
   command_options();
   int i = 0;
   while(1){
     std::getline(std::cin,cmd);
-    if(i>=9)
+    if(i>8)
     {
-      std::cout << "\x1b[93;42m**********************************************\n";
-      std::cout << "\x1b[93;42m***                                        ***\n";
-      std::cout << "\x1b[93;42m***            PHONE BOOK IS FULL          ***\n";
-      std::cout << "\x1b[93;42m***                                        ***\n";
-      std::cout << "\x1b[93;42m**********************************************\n";  
+      std::cout << "\x1b[1;42m**********************************************\n";
+      std::cout << "\x1b[1;42m***                                        ***\n";
+      std::cout << "\x1b[1;42m***            PHONE BOOK IS FULL          ***\n";
+      std::cout << "\x1b[1;42m***                                        ***\n";
+      std::cout << "\x1b[1;42m**********************************************\n";  
     }
-    if(cmd == "ADD" && i<=8){
-      new_contact.addContact();
+    if(cmd == "ADD" && i<=7){
+      new_contact[i].addContact();
       i++;
     }
     else if(cmd == "SEARCH"){
@@ -86,11 +86,11 @@ int main() {
       break;
     }
     else{
-      std::cout << "\x1b[93;42m**********************************************\n";
-      std::cout << "\x1b[93;42m*** ENTER IN ONE OF THE FOLLOWING COMMANDS ***\n";
-      std::cout << "\x1b[93;42m***          <<COMMAND OPTIONS>>           ***\n";
-      std::cout << "\x1b[93;42m***            ADD SERCH EXIT              ***\n";
-      std::cout << "\x1b[93;42m**********************************************\n";    
+      std::cout << "\x1b[1;42m**********************************************\n";
+      std::cout << "\x1b[1;42m*** ENTER IN ONE OF THE FOLLOWING COMMANDS ***\n";
+      std::cout << "\x1b[1;42m***          <<COMMAND OPTIONS>>           ***\n";
+      std::cout << "\x1b[1;42m***            ADD SERCH EXIT              ***\n";
+      std::cout << "\x1b[1;42m**********************************************\n";    
      }
 
     }
