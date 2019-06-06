5a9467dd373aa8e50e5dc465fae461bce778ce1f
Rutendo Nyakutira <rnyakuti@student.wethinkcode.co.za>
2019-06-06 23:02:10 +0200

    day 03 started

---
+++ Day03/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+/*class FragTrap
+{
+	
+};*/
+
+static void menu()
+{
+	
+	std::cout << "\x1b[36;1m            000000000000\x1b[0m\n";
+	std::cout << "\x1b[36;1m          0000000000000000\x1b[0m\n";
+	std::cout << "\x1b[36;1m        00000            00000\x1b[0m\n";
+	std::cout << "\x1b[36;1m      000000               000000\x1b[0m\n";
+	std::cout << "\x1b[36;1m	 00000                   00000\x1b[0m\n";
+	std::cout << "\x1b[36;1m	000                         000\x1b[0m\n";
+	std::cout << "\x1b[36;1m  000              000           000\x1b[0m\n";
+	std::cout << "\x1b[36;1m 000              00000           000\x1b[0m\n";
+	std::cout << "\x1b[36;1m000             000  000           000\x1b[0m\n";
+	std::cout << "\x1b[36;1m000            000    000           000\x1b[0m\n";
+	std::cout << "\x1b[36;1m000           000      000          000\x1b[0m\n";
+	std::cout << "\x1b[36;1m000          000        000         000\x1b[0m\n";
+	std::cout << "\x1b[36;1m000         000          000        000\x1b[0m\n";
+	std::cout << "\x1b[36;1m000        000            000       000\x1b[0m\n";
+	std::cout << "\x1b[36;1m0000      000              000     0000\x1b[0m\n";
+	std::cout << "\x1b[36;1m 0000    0000              0000    0000\x1b[0m\n";
+	std::cout << "\x1b[36;1m  0000000000                00000000000\x1b[0m\n";
+	std::cout << "\x1b[36;1m   00000000000           0000000000000\x1b[0m\n";
+	std::cout << "\x1b[36;1m    00000000000000000000000000000000\x1b[0m\n";
+	std::cout << "\x1b[36;1m     00000000000000000000000000000\x1b[0m\n";
+	std::cout << "\x1b[36;1m	   0000000000000000000000000\x1b[0m\n";
+	std::cout << "\x1b[36;1m	       0000000000000000000\x1b[0m\n";
+	std::cout << "\x1b[36;1m		        000000000\x1b[0m\n";
+	std::cout << "\x1b[36;1m\x1b[0m\n";
+	std::cout << "\x1b[36;1m		    	W E L C O M E \x1b[0m\n";
+	std::cout << "\x1b[36;1m					 T O\x1b[0m\n";
+	std::cout << "\x1b[36;1m				    T H E\x1b[0m\n";
+	std::cout << "\x1b[36;1m             B O D E R L A N D S\x1b[0m\n";
+	std::cout << "\x1b[36;1m\x1b[0m\n";
+}
+int main()
+{
+	menu();
+	return (0);
+}
